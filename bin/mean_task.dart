0f694e38431a64ae7e4097c31da2958b739ea3e2
Ammar Al Hasani <ammarsalem2011@gmail.com>
2023-08-28 14:10:07 +0400

    Initial commit

---
+++ bin/mean_task.dart
@@ -0,0 +1,11 @@
+import 'package:mean_task/mean_task.dart' as mean_task;
+
+void main() {
+  double? minx=28;
+  double?x=6;
+  double? miny=27;
+  double? y=7;
+  double sevencomp=y*miny-x*minx;
+  print('The points of 7th compititor=${sevencomp.toInt()}');
+
+}
---
+++ test/mean_task_test.dart
@@ -0,0 +1,8 @@
+import 'package:mean_task/mean_task.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
